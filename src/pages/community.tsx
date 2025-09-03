4f7b2255879a24b329b863ae5ac52250ef0822d5
M Chandramohan <mohan0265@gmail.com>
2025-09-03 11:28:07 +0800

    Update MyDurhamLaw

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,19 +1,25 @@
+// src/pages/community.tsx
 import React from "react";
-import { HeroSection } from "../components/community/HeroSection";
-import { CategoryTabs } from "../components/community/CategoryTabs";
-import { EmergencyEssentialsSection } from "../components/community/EmergencyEssentialsSection";
-import { HealthcareSection } from "../components/community/HealthcareSection";
-import { TransportSection } from "../components/community/TransportSection";
-import { PostAndGovSection } from "../components/community/PostAndGovSection";
-import { DiningSection } from "../components/community/DiningSection";
-import { EventsCarousel } from "../components/community/EventsCarousel";
-import { MapSection } from "../components/community/MapSection";
-import { SafetyTipsSection } from "../components/community/SafetyTipsSection";
-import { StudentSocialCard } from "../components/community/StudentSocialCard";
+import {
+  HeroSection,
+  CategoryTabs,
+  EmergencyEssentialsSection,
+  HealthcareSection,
+  TransportSection,
+  PostAndGovSection,
+  DiningSection,
+  EventsCarousel,
+  MapSection,
+  SafetyTipsSection,
+  StudentSocialCard,
+} from "@/components/community";
 
 export default function CommunityPage() {
   return (
-    <main className="container mx-auto max-w-4xl px-2 sm:px-6 py-10" aria-label="Durham Community page">
+    <main
+      className="container mx-auto max-w-6xl px-4 sm:px-6 py-10"
+      aria-label="Durham Community page"
+    >
       <HeroSection />
       <CategoryTabs />
       <EmergencyEssentialsSection />
