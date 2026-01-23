23d575e7dcc0bdd5503b508691697bed03b36680
M Chandramohan <mohan0265@gmail.com>
2026-01-24 00:22:10 +0800

    Update MyDurhamLaw

--- src/components/GlobalHeader.tsx
+++ src/components/GlobalHeader.tsx
@@ -196,6 +196,7 @@ export default function GlobalHeader() {
     () => ({
       label: 'Study',
       items: [
+        { label: 'Learn Durham Law', href: '/learn' },
         { label: 'Year at a Glance', href: '/year-at-a-glance' },
         { label: 'Assignments', href: '/assignments' },
         { label: 'My Lectures', href: '/study/lectures' },
